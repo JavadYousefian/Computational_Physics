55c31b9394f966fefcab2b113ee237cecd33686a
Javad Yousefian <javadyousefian@mac.mynetworksettings.com>
2026-09-07 01:33:17 -0400

    Initial Varibles and some packages were added

---
+++ Falling_Cow/falling_cow.py
@@ -0,0 +1,18 @@
+# Importing Packages
+import math
+import matplotlib.pyplot as plt
+
+
+# Initial Conditions
+
+v_x = 1
+v_y = 100
+drag = 0
+x_0 = 0
+y_0 = 1000
+time_step = 0.001
+g = 9.8
+
+
+
+

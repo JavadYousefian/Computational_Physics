90ff94a1b67ee13e0d167e2c11b53ca565feb8a2
Javad Yousefian <javadyousefian@mac.mynetworksettings.com>
2026-09-07 01:44:44 -0400

    Force function and more variables added

--- Falling_Cow/falling_cow.py
+++ Falling_Cow/falling_cow.py
@@ -4,14 +4,27 @@ import matplotlib.pyplot as plt
 
 
 # Initial Conditions
-
 v_x = 1
 v_y = 100
 drag = 0
 x_0 = 0
 y_0 = 1000
 time_step = 0.001
+t = 0
+mass = 1000
 g = 9.8
+
+# Force Function
+def get_force(x_0, y_0, v_x, v_y, mass, drag):
+
+    f_x = 0
+    f_y = - mass * g
+
+    speed = math.sqrt(v_x**2 + v_y**2)
+    f_x = f_x - drag * speed * v_x
+    f_y = f_y - drag * speed * v_y
+
+    return f_x, f_y
 
 
 

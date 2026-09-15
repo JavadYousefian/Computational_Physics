7c34bbb2900bcf14784d0d0119de76bbd592951d
Javad Yousefian <javadyousefian@Javads-MacBook-Pro.local>
2026-09-15 13:35:08 -0400

    Euler explicit added

--- Spring_Oscillator/code.py
+++ Spring_Oscillator/code.py
@@ -14,3 +14,21 @@ v_0 = 0.0
 
 # initial energy (for comparison later)
 E_0 = 0.5 * m * v_0**2 + 0.5 * k * x_0**2
+
+def euler_explicit(x_0, v_0, dt, N):
+    x = np.empty(N + 1)
+    v = np.empty(N + 1)
+    x[0] = x_0
+    v[0] = v_0
+    for i in range(N):
+        # force / mass
+        a = -(k / m) * x[i] 
+        # uses OLD v          
+        x[i+1] = x[i] + v[i] * dt 
+        # uses OLD x (through a)    
+        v[i+1] = v[i] + a * dt        
+    return x, v
+
+# Force function
+def calc_force(x, k):
+    return (1/2 * k * (x**2))

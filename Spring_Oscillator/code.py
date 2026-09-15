4a5f2d6748e9e793c4286f74da03d77b524fc93b
Javad Yousefian <javadyousefian@Javads-MacBook-Pro.local>
2026-09-15 13:43:27 -0400

    Energy function and euler symplectic function added

--- Spring_Oscillator/code.py
+++ Spring_Oscillator/code.py
@@ -15,6 +15,8 @@ v_0 = 0.0
 # initial energy (for comparison later)
 E_0 = 0.5 * m * v_0**2 + 0.5 * k * x_0**2
 
+
+# Euler Explicit function 
 def euler_explicit(x_0, v_0, dt, N):
     x = np.empty(N + 1)
     v = np.empty(N + 1)
@@ -26,9 +28,32 @@ def euler_explicit(x_0, v_0, dt, N):
         # uses OLD v          
         x[i+1] = x[i] + v[i] * dt 
         # uses OLD x (through a)    
-        v[i+1] = v[i] + a * dt        
+        v[i+1] = v[i] + a * dt  
+
+    # So it updated the list and get us x and v for steps number we have      
     return x, v
 
+
+# Euler Symplectic function
+def euler_symplectic(x_0, v_0, dt, N):
+    x = np.empty(N + 1)
+    v = np.empty(N + 1)
+    x[0] = x_0
+    v[0] = v_0
+    for i in range(N):
+        v[i+1] = v[i] - (k / m) * x[i] * dt  
+
+        # drift with new v
+        x[i+1] = x[i] + v[i+1] * dt     
+
+    # like the previous function, it updates the list and get us x and v for steps number we have       
+    return x, v
+
+
 # Force function
-def calc_force(x, k):
-    return (1/2 * k * (x**2))
+def calc_force(x = 0,v = 0, initial_t = 0):
+    return (-k * x)
+
+# Energy function
+def energy(x, v):
+    return 0.5 * m * v**2 + 0.5 * k * x**2

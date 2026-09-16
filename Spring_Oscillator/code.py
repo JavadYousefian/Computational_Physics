f00a3254191190733498d328ffe4dcfb951315f6
Javad Yousefian <javadyousefian@mac.mynetworksettings.com>
2026-09-16 15:34:18 -0400

    First plot added

--- Spring_Oscillator/code.py
+++ Spring_Oscillator/code.py
@@ -17,7 +17,7 @@ E_0 = 0.5 * m * v_0**2 + 0.5 * k * x_0**2
 
 
 # Euler Explicit function 
-def euler_explicit(x_0, v_0, dt, N):
+def calc_euler_explicit(x_0, v_0, dt, N):
     x = np.empty(N + 1)
     v = np.empty(N + 1)
     x[0] = x_0
@@ -35,7 +35,7 @@ def euler_explicit(x_0, v_0, dt, N):
 
 
 # Euler Symplectic function
-def euler_symplectic(x_0, v_0, dt, N):
+def calc_euler_symplectic(x_0, v_0, dt, N):
     x = np.empty(N + 1)
     v = np.empty(N + 1)
     x[0] = x_0
@@ -49,6 +49,28 @@ def euler_symplectic(x_0, v_0, dt, N):
     # like the previous function, it updates the list and get us x and v for steps number we have       
     return x, v
 
+def runge_kutta_second_order(x_0, v_0, dt, N):
+    x = np.empty(N + 1)
+    v = np.empty(N + 1)
+    x[0] = x_0
+    v[0] = v_0
+    for i in range(N):
+        # slope at start
+        k1_x = v[i]
+        k1_v = -(k / m) * x[i]
+        # position and velocity at the midpoint
+        x_m = x[i] + 0.5 * dt * k1_x
+        v_m = v[i] + 0.5 * dt * k1_v
+        # slope at midpoint
+        k2_x = v_m
+        k2_v = -(k / m) * x_m
+        # full step using midpoint slope
+        v[i+1] = v[i] + dt * k2_v
+        x[i+1] = x[i] + dt * k2_x
+
+
+    # Just get x and y updated based on second order runge kutta algorithm
+    return x, v
 
 # Force function
 def calc_force(x = 0,v = 0, initial_t = 0):
@@ -57,3 +79,32 @@ def calc_force(x = 0,v = 0, initial_t = 0):
 # Energy function
 def energy(x, v):
     return 0.5 * m * v**2 + 0.5 * k * x**2
+
+# Step size and total time
+dt = 0.01
+total_time = 60
+step = int(total_time / dt)
+t = np.linspace(0, step * dt, step + 1)
+
+# Run each integrator
+x1, v1 = calc_euler_explicit(x_0, v_0, dt, step)
+x2, v2 = calc_euler_symplectic(x_0, v_0, dt, step)
+x3, v3 = runge_kutta_second_order(x_0, v_0, dt, step)
+
+# Exact solution for comparison for our oscillator
+x_true = x_0 * np.cos(omega * t)
+
+# plot x(t)
+# note: I ran the code and when you zoom it is showing that exact and RK2 are nearly close rather than other methods
+plt.figure(figsize=(10,4))
+plt.plot(t, x_true, 'k--', label='exact', alpha=0.6)
+plt.plot(t, x1, label='Euler explicit')
+plt.plot(t, x2, label='Euler symplectic')
+plt.plot(t, x3, label='Runge Kutta second_order')
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.title('position vs time')
+plt.legend()
+plt.savefig("Position vs Time.png")
+plt.grid(alpha=0.3)
+plt.show()
